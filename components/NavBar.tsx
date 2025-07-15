cd3eb0fa1481482ace500d72be9ab0727563cad5
Adarshtheki <adarshverma549@gmail.com>
2025-07-15 15:19:05 +0530

    update layout and logo name, enhance navbar with search functionality, and improve address card component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,19 +1,21 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { UserButton, useUser } from '@clerk/nextjs';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import {
-    CircleUserRound,
-    Menu,
-    ShoppingCart,
-    Home,
-    ShoppingBasket,
-    Heart,
-    Contact,
-    FileClock,
-    MessageCircle,
+  Menu,
+  ShoppingCart,
+  Home,
+  ShoppingBasket,
+  Heart,
+  Contact,
+  FileClock,
+  MessageCircle,
+  Search,
+  User,
+  X,
 } from 'lucide-react';
 
 import SearchSection from './SearchSection';
@@ -22,79 +24,104 @@ import useDropdown from '@/utils/useDropdown';
 import LogoSvg from '../utils/LogoSvg';
 
 const NavBar = () => {
-    const { user } = useUser();
-    const cart = useCart();
-    const pathname = usePathname();
-    const { isOpen, toggle, dropdownRef } = useDropdown();
+  const { user } = useUser();
+  const cart = useCart();
+  const pathname = usePathname();
+  const { isOpen, toggle, dropdownRef } = useDropdown();
+  const [isOpenSearch, setIsOpenSearch] = useState(false);
 
-    return (
-        <div className='sticky top-0 z-10 py-2 px-10 flex gap-2 text-base-medium text-gray-700 justify-between items-center bg-white max-sm:px-2'>
+  return (
+    <>
+      <SearchSection setOpenSearch={setIsOpenSearch} openSearch={isOpenSearch} />
+      <div className='backdrop-blur-2xl shadow-md sticky top-0 left-0 w-full z-10'>
+        <div className='container mx-auto px-4'>
+          <div className='flex items-center justify-between py-4'>
             <Link href='/'>
-                <LogoSvg />
+              <LogoSvg />
             </Link>
 
+            <nav className='max-sm:hidden'>
+              <ul className='flex items-center gap-4 capitalize'>
+                {navLinks.map((link, index) => (
+                  <li key={index}>
+                    <Link href={link.href}>{link.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+
             {/* Right Section */}
-            <div className='flex items-center sm:gap-5 gap-2'>
-                <SearchSection />
+            <div className='flex items-center sm:gap-5 gap-3'>
+              <Search onClick={() => setIsOpenSearch(true)} className='cursor-pointer' />
 
-                <Menu className='cursor-pointer' onClick={toggle} />
-                {isOpen && (
-                    <div
-                        ref={dropdownRef}
-                        className='absolute text-base-medium w-[80vw] max-w-[400px] overflow-hidden sm:right-10 right-5 top-12 bg-white border shadow-2xl rounded-2xl'>
-                        <div className='flex z-50 flex-col'>
-                            {/* <Link
-                                        href={user?.emailAddresses ? '/wishlist' : '/sign-in'}
-                                        className='hover:text-red-1 hover:bg-gray-200 p-5'>
-                                        Wishlist
-                                    </Link> */}
-                            {navLinks.map((link) => {
-                                return (
-                                    <Link
-                                        onClick={toggle}
-                                        href={
-                                            ['wishlist', 'orders', 'cart', 'comment'].includes(
-                                                link.name
-                                            )
-                                                ? user?.emailAddresses
-                                                    ? link.href
-                                                    : '/sign-in'
-                                                : link.href
-                                        }
-                                        key={link.name}
-                                        className='hover:text-red-1 flex items-center gap-4 hover:bg-gray-200 capitalize py-3 border-b'>
-                                        <span className='pl-10'>{link.icon}</span>
-                                        {link.name}
-                                    </Link>
-                                );
-                            })}
-                        </div>
-                    </div>
-                )}
-
-                {/* User Section */}
-                <div>
-                    {user?.emailAddresses ? (
-                        <UserButton afterSignOutUrl='/sign-in' />
-                    ) : (
-                        <Link href={'/sign-in'}>
-                            <CircleUserRound />
+              {isOpen && (
+                <div
+                  ref={dropdownRef}
+                  className='absolute text-base-medium w-[80vw] max-w-[400px] overflow-hidden sm:right-10 right-5 top-16 bg-white border shadow-2xl rounded-2xl'>
+                  <div className='flex z-50 flex-col'>
+                    {navLinks.map((link) => {
+                      return (
+                        <Link
+                          onClick={toggle}
+                          href={
+                            ['wishlist', 'orders', 'cart', 'comment'].includes(link.name)
+                              ? user?.emailAddresses
+                                ? link.href
+                                : '/sign-in'
+                              : link.href
+                          }
+                          key={link.name}
+                          className='hover:text-red-1 flex items-center gap-4 hover:bg-gray-200 capitalize py-3 border-b'>
+                          <span className='pl-10'>{link.icon}</span>
+                          {link.name}
                         </Link>
-                    )}
+                      );
+                    })}
+                  </div>
                 </div>
+              )}
+
+              <Link href='/cart' aria-label='Shopping Cart' className='relative'>
+                <ShoppingCart />
+                {!!cart?.cartItems?.length && (
+                  <span className='absolute -top-2 -right-2 bg-indigo-600 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center'>
+                    {cart.cartItems.length}
+                  </span>
+                )}
+              </Link>
+
+              <Link href={'/wishlist'}>
+                <Heart />
+              </Link>
+
+              {/* User Section */}
+              {user?.emailAddresses ? (
+                <UserButton afterSignOutUrl='/sign-in' />
+              ) : (
+                <Link href={'/sign-in'}>
+                  <User />
+                </Link>
+              )}
+
+              {!!isOpen ? (
+                <X className='cursor-pointer sm:hidden' onClick={toggle} />
+              ) : (
+                <Menu className='cursor-pointer sm:hidden' onClick={toggle} />
+              )}
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
 
 const navLinks = [
-    { href: '/', icon: <Home />, name: 'home' },
-    { href: '/products', icon: <ShoppingBasket />, name: 'products' },
-    { href: '/contact', icon: <Contact />, name: 'contact' },
-    { href: '/cart', icon: <ShoppingCart />, name: 'cart' },
-    { href: '/wishlist', icon: <Heart />, name: 'wishlist' },
-    { href: '/orders', icon: <FileClock />, name: 'orders' },
-    { href: '/reviews', icon: <MessageCircle />, name: 'comment' },
+  { href: '/', icon: <Home />, name: 'home' },
+  { href: '/products', icon: <ShoppingBasket />, name: 'products' },
+  { href: '/contact', icon: <Contact />, name: 'contact' },
+  { href: '/orders', icon: <FileClock />, name: 'orders' },
+  { href: '/reviews', icon: <MessageCircle />, name: 'comment' },
 ];
--- components/SearchSection.tsx
+++ components/SearchSection.tsx
@@ -7,152 +7,135 @@ import { useRouter } from 'next/navigation';
 import useDropdown from '@/utils/useDropdown';
 import useFetch from '@/utils/useFetch';
 
-const SearchSection = () => {
-    const router = useRouter();
-    const { isOpen, toggle, dropdownRef } = useDropdown();
-    const [query, setQuery] = useState<string>('');
-    const [products, setProducts] = useState<ProductType[]>([]);
-    const [loading, setLoading] = useState<boolean>(false);
-    const { data } = useFetch(`${process.env.NEXT_PUBLIC_API_URL}/products?sortBy=rating`);
+type SearchSectionType = {
+  openSearch: boolean;
+  setOpenSearch: (val: boolean) => void;
+};
 
-    const fetchData = async (query: string) => {
-        setLoading(true);
-        try {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/search?q=${query}`);
-            const data = await response.json();
-            setProducts(data);
-        } catch (error) {
-            console.error('Error fetching products:', error);
-        }
-        setLoading(false);
+const SearchSection = ({ openSearch, setOpenSearch }: SearchSectionType) => {
+  const router = useRouter();
+  const [query, setQuery] = useState<string>('');
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const { data } = useFetch(`${process.env.NEXT_PUBLIC_API_URL}/products?sortBy=rating`);
+
+  const fetchData = async (query: string) => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/search?q=${query}`);
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      if (query) {
+        fetchData(query);
+      }
+    }, 1000);
+
+    return () => {
+      clearTimeout(handler);
     };
+  }, [query]);
 
-    useEffect(() => {
-        const handler = setTimeout(() => {
-            if (query) {
-                fetchData(query);
-            }
-        }, 1000);
+  const handleGoProduct = (id: string) => {
+    router.push(`/products/${id}`);
+    setOpenSearch(false);
+  };
 
-        return () => {
-            clearTimeout(handler);
-        };
-    }, [query]);
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (query.length === 0) return;
+    router.push(`/search/${query}`);
+    setQuery('');
+    setOpenSearch(false);
+  };
 
-    const handleGoProduct = useCallback(
-        (id: string) => {
-            router.push(`/products/${id}`);
-            toggle();
-        },
-        [router, toggle]
-    );
+  if (!openSearch) return null;
 
-    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (query.length === 0) return;
-        router.push(`/search/${query}`);
-        setQuery('');
-        toggle();
-    };
-
-    return (
-        <>
-            <div className='relative'>
-                {/* Search button */}
-                <button onClick={toggle} className='flex items-center p-1'>
-                    <Search />
-                    <span className='hidden sm:block'>Search...</span>
-                </button>
-                {/* Search Dropdown */}
-                {isOpen && (
-                    <div className='fixed inset-0 z-50 flex items-end justify-center bg-black bg-opacity-40'>
-                        <div
-                            ref={dropdownRef}
-                            className='rounded-2xl overflow-hidden shadow-lg w-full h-[90%] max-w-[800px] bg-gray-100'>
-                            <form
-                                onSubmit={handleSearch}
-                                className='flex z-10 sticky top-0 w-full flex-row items-start justify-between gap-5 bg-white p-2'>
-                                <div className='flex w-full flex-row items-center sm:gap-5'>
-                                    <span
-                                        onClick={toggle}
-                                        className='p-2 cursor-pointer rounded-lg hover:bg-gray-200'>
-                                        <MoveLeft />
-                                    </span>
-                                    <input
-                                        value={query}
-                                        onChange={(e) => setQuery(e.target.value)}
-                                        placeholder='Search for Mattresses, Beds, Sofas, etc'
-                                        className='p-2 rounded-full border active:border-blue-1 w-full'
-                                    />
-                                </div>
-                                <button
-                                    type='submit'
-                                    className='p-2 rounded-lg bg-blue-1 text-white'>
-                                    <Search />
-                                </button>
-                            </form>
-                            <div className='overflow-y-scroll h-full pb-5'>
-                                {loading ? (
-                                    <div className='flex justify-center items-center p-4'>
-                                        Loading...
-                                    </div>
-                                ) : (
-                                    query &&
-                                    products.map((product: ProductType) => (
-                                        <div
-                                            onClick={() => handleGoProduct(product._id)}
-                                            key={product._id}
-                                            className='flex gap-4 items-center px-4 hover:bg-grey-1/20 cursor-pointer'>
-                                            <Image
-                                                src={product.thumbnail || '/placeholder.jpg'}
-                                                alt='coverImage'
-                                                width={40}
-                                                height={20}
-                                                className='object-contain'
-                                            />
-                                            <p>{product.title}</p>
-                                        </div>
-                                    ))
-                                )}
-                                <p className='p-4 text-base-bold '>Popular Products</p>
-                                <div className='px-4 grid md:grid-cols-3 grid-cols-2 gap-2'>
-                                    {data &&
-                                        data?.products?.map((product: ProductType) => (
-                                            <div
-                                                onClick={() => handleGoProduct(product._id)}
-                                                key={product._id}
-                                                className='sm:flex gap-2 md:scale-100 scale-70 items-center hover:bg-grey-1/20 cursor-pointer'>
-                                                <Image
-                                                    src={product.thumbnail || '/placeholder.jpg'}
-                                                    alt='coverImage'
-                                                    width={60}
-                                                    height={30}
-                                                    className='object-contain'
-                                                />
-                                                <div className='text-grey-1 p-2 flex flex-col'>
-                                                    <span className='text-small-bold'>
-                                                        {product?.title}
-                                                    </span>
-                                                    <small>{product?.category}</small>
-                                                    <small>
-                                                        Price:{' '}
-                                                        {product?.price?.toLocaleString('en-US', {
-                                                            maximumFractionDigits: 0,
-                                                            style: 'currency',
-                                                            currency: 'USD',
-                                                        })}
-                                                    </small>
-                                                </div>
-                                            </div>
-                                        ))}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )}
-            </div>
-        </>
-    );
+  return (
+    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-40'>
+      <div className='rounded-2xl overflow-hidden shadow-lg sm:h-[80dvh] sm:w-[80dvw] w-full h-full bg-gray-100'>
+        <form
+          onSubmit={handleSearch}
+          className='flex z-10 sticky top-0 w-full flex-row items-start justify-between gap-5 bg-white p-2'>
+          <div className='flex w-full flex-row items-center sm:gap-5'>
+            <span
+              onClick={() => setOpenSearch(false)}
+              className='p-2 cursor-pointer rounded-lg hover:bg-gray-200'>
+              <MoveLeft />
+            </span>
+            <input
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder='Search for Mattresses, Beds, Sofas, etc'
+              className='p-2 rounded-full border active:border-blue-1 w-full'
+            />
+          </div>
+          <button type='submit' className='p-2 rounded-lg bg-blue-1 text-white'>
+            <Search />
+          </button>
+        </form>
+        <div className='overflow-y-scroll h-full pb-5'>
+          {loading ? (
+            <div className='flex justify-center items-center p-4'>Loading...</div>
+          ) : (
+            query &&
+            products.map((product: ProductType) => (
+              <div
+                onClick={() => handleGoProduct(product._id)}
+                key={product._id}
+                className='flex gap-4 items-center px-4 hover:bg-grey-1/20 cursor-pointer'>
+                <Image
+                  src={product.thumbnail || '/placeholder.jpg'}
+                  alt='coverImage'
+                  width={40}
+                  height={20}
+                  className='object-contain'
+                />
+                <p>{product.title}</p>
+              </div>
+            ))
+          )}
+          <p className='p-4 text-base-bold '>Popular Products</p>
+          <div className='px-4 grid md:grid-cols-3 grid-cols-2 gap-2'>
+            {data &&
+              data?.products?.map((product: ProductType) => (
+                <div
+                  onClick={() => handleGoProduct(product._id)}
+                  key={product._id}
+                  className='sm:flex gap-2 md:scale-100 scale-70 items-center hover:bg-grey-1/20 cursor-pointer'>
+                  <Image
+                    src={product.thumbnail || '/placeholder.jpg'}
+                    alt='coverImage'
+                    width={60}
+                    height={30}
+                    className='object-contain'
+                  />
+                  <div className='text-grey-1 p-2 flex flex-col'>
+                    <span className='text-small-bold'>{product?.title}</span>
+                    <small>{product?.category}</small>
+                    <small>
+                      Price:{' '}
+                      {product?.price?.toLocaleString('en-US', {
+                        maximumFractionDigits: 0,
+                        style: 'currency',
+                        currency: 'USD',
+                      })}
+                    </small>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchSection;
